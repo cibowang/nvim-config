a51c94acbcb18b7fb7865e9289bf1a9674511116
cibowang <980368160@qq.com>
2025-10-21 13:12:03 +0800

    16th commit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -41,23 +41,6 @@ return {
   },
 
   {
-    'saecki/crates.nvim',
-    ft = {"toml"},
-    config = function()
-      require("crates").setup {
-        completion = {
-          cmp = {
-            enabled = true
-          },
-        },
-      }
-      require('cmp').setup.buffer({
-        sources = { { name = "crates" }}
-      })
-    end
-  },
-
-  {
     "rust-lang/rust.vim",
     ft = "rust",
     init = function ()
